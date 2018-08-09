94ca7d8ddba1dbf7c87af25addb67b970e43e466
Leslie Turis <lturis@gmail.com>
2018-08-09 00:23:42 +0000

    Done.

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const climbRanks = document.querySelectorAll('.ranked-list');
-
   for (let i = 0; i < climbRanks.length; i++) {
-      let children = climbRanks[i].children;
-      for (let j = 0; j < climbRanks.length; j++) {
-        children = parseInt(children[j].innerHTML) + n;
-      }
+      climbRanks[i].innerHTML = parseInt(climbRanks[i].innerHTML) + n;
   }
+}
+
+function deepestChild() {
+  const ele = document.querySelector('#grand-node div');
+  while (ele) {
+    let mostDeep = ele.childNodes[ele.childNodes.length - 1];
+    if (mostDeep === ele.lastElementChild) {
+      return dee;
+    }
+  }
+  return null;
 }
